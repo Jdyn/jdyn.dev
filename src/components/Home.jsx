a2c6d8132933381838a2c40b727ae71a35969d74
Jdyn <moorejaden5389@gmail.com>
2019-05-07 13:07:10 -0700

    sitemap?
    is this good

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,7 +32,7 @@ const Home = props => {
       {/* <Languages languages={props.languages} /> */}
       <TechStack cards={cards} />
       <Projects projects={projects} />
-      <footer className={classes.footer}>updated {formatTime(1557173555331)}</footer>
+      <footer className={classes.footer}>updated {formatTime(1557259615624)}</footer>
     </div>
   );
 };
