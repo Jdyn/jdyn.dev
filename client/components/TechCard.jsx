0c3efeb5ca5c1aaa36285a89e23bfaa3f59da3a3
Jdyn <moorejaden5389@gmail.com>
2019-09-02 20:23:56 -0700

    package fixes

--- client/components/TechCard.jsx
+++ client/components/TechCard.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import withStyles from "react-jss";
-import { animated, interpolate } from "react-spring";
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from 'react-jss';
+import { animated, interpolate } from 'react-spring';
 
 const propTypes = {
   card: PropTypes.object.isRequired,
@@ -13,7 +13,7 @@ const propTypes = {
 };
 
 const trans = (rotation, scale) =>
-  `perspective(${"1500px"}) rotateX(30deg) rotateY(${rotation /
+  `perspective(${'1500px'}) rotateX(30deg) rotateY(${rotation /
     10}deg) rotateZ(${rotation}deg) scale(${scale})`;
 
 const TechCard = props => {
@@ -38,63 +38,63 @@ const TechCard = props => {
 
 const styles = theme => ({
   root: {
-    position: "relative",
-    width: "100%",
-    maxWidth: "285px",
-    height: "100%",
+    position: 'relative',
+    width: '100%',
+    maxWidth: '285px',
+    height: '100%',
     borderRadius: 10,
-    cursor: "grab",
+    cursor: 'grab',
     backgroundColor: theme.primary,
     boxShadow: `0 12.5px 50px -10px ${theme.shadow}, 0 10px 10px -10px ${theme.shadow}`,
-    "& p": {
+    '& p': {
       margin: 0,
-      padding: "20px",
-      userSelect: "none",
+      padding: '20px',
+      userSelect: 'none',
       fontSize: 17,
       flexGrow: 1,
       color: theme.color,
       fontWeight: 500,
-      letterSpacing: "0.085em"
+      letterSpacing: '0.085em'
     }
   },
   container: {
-    display: "flex",
-    flexDirection: "column",
-    position: "relative",
-    width: "100%",
-    height: "100%",
-    "& *": {
-      WebkitTouchCallout: "none",
-      WebkitUserSelect: "none"
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    '& *': {
+      WebkitTouchCallout: 'none',
+      WebkitUserSelect: 'none'
     }
   },
   header: {
-    display: "flex",
-    position: "relative",
-    flexDirection: "column",
-    height: "195px",
-    borderRadius: "10px 10px 0 0",
+    display: 'flex',
+    position: 'relative',
+    flexDirection: 'column',
+    height: '195px',
+    borderRadius: '10px 10px 0 0',
     backgroundColor: theme.tertiary,
     color: theme.color,
-    "& img": {
-      position: "relative",
-      maxWidth: "35%",
-      height: "60%",
-      userSelect: "none",
-      marginTop: "5%",
-      marginLeft: "5%"
+    '& img': {
+      position: 'relative',
+      maxWidth: '35%',
+      height: '60%',
+      userSelect: 'none',
+      marginTop: '5%',
+      marginLeft: '5%'
     },
-    "& h2": {
-      display: "inline-flex",
-      margin: "5px 15px 5px 15px",
+    '& h2': {
+      display: 'inline-flex',
+      margin: '5px 15px 5px 15px',
       fontSize: 20,
       fontWeight: 700,
-      textTransform: "uppercase",
-      letterSpacing: "0.125em",
-      alignItems: "center",
-      "& span": {
-        textAlign: "right",
-        width: "100%"
+      textTransform: 'uppercase',
+      letterSpacing: '0.125em',
+      alignItems: 'center',
+      '& span': {
+        textAlign: 'right',
+        width: '100%'
       }
     }
   }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const next = require("next");
-const { createServer } = require("http");
+const next = require('next');
+const { createServer } = require('http');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev: true, dir: "client/" });
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: 'client/' });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
