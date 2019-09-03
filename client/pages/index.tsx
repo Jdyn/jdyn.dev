9007885aa26554992a1f5236d3d3cba063fc7748
Jaden <moorejaden5389@gmail.com>
2019-09-03 15:06:33 -0700

    Convert Home to lambda

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { NextPageContext } from 'next';
 import HomeContainer from '../containers/HomeContainer';
 
 class Index extends React.PureComponent {
+  public static async getInitialProps(ctx: NextPageContext): Promise<object> {
+    return {};
+  }
+
   public render(): JSX.Element {
     return <HomeContainer {...this.props} />;
   }
