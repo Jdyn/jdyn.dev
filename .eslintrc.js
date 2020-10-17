a6966a65c7cdd7d6c860793437e19383e4cd4108
Jaden <moorejaden5389@gmail.com>
2020-10-17 10:37:09 -0700

    remove now config

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier', '@typescript-eslint'],
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': [0],
+    'react/prop-types': [0],
+    'jsx-a11y/anchor-is-valid': [0],
+    '@typescript-eslint/no-empty-function': 'off',
+    'jsx-a11y/href-no-hash': ['off'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.tsx'] }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['warn']
+      }
+    }
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.module.css']
+      }
+    }
+  }
+};
